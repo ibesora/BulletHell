4f284d9da8942d05ca5b08b06d896f2934baf2e3
ibesora <isaac.besora@gmail.com>
2021-09-17 12:10:01 +0200

    Add project files.

---
+++ BulletHell.cpp
@@ -0,0 +1,38 @@
+#include "raylib.h"
+
+const int ScreenWidth = 1280;
+const int ScreenHeight = 720;
+
+int main(void) {
+
+    InitWindow(ScreenWidth, ScreenHeight, "Bullet Hell");
+
+    SetTargetFPS(60);
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
