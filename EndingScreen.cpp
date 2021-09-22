bd5643f26e483024cd8379a44aaab3ed0dc608be
ibesora <isaac.besora@gmail.com>
2021-09-22 12:58:29 +0200

    Adds ending screen

--- EndingScreen.cpp
+++ EndingScreen.cpp
@@ -1,12 +1,46 @@
 #include "raylib.h"
 #include "EndingScreen.h"
+#include "GameplayScreen.h"
+#include "TitleScreen.h"
+#include "GameStatus.h"
+#include "Animations.h"
+#include <stdlib.h>
 
-EndingScreen::EndingScreen() : Screen(ScreenType::Ending) {
+const int FadeInFramesNum = 60;
+const int VisibleFramesNum = 0;
+const int FadeOutFramesNum = 0;
+const int FadeOutStartFramesNum = FadeInFramesNum + VisibleFramesNum;
+const int TotalFramesNum = FadeInFramesNum + VisibleFramesNum + FadeOutFramesNum;
+
+EndingScreen::EndingScreen() : Screen(ScreenType::Pause) {
+    this->currentFrame = 0;
+    this->currentSelectedOption = Option::NextRun;
+}
+
+void EndingScreen::updateGameStatus(GameStatus *current) {
+    if (this->currentFrame >= TotalFramesNum) {
+        const int selectedOption = static_cast<int>(this->currentSelectedOption);
+        const int optionNum = static_cast<int>(Option::End);
+        if (IsKeyPressed(KEY_DOWN)) this->currentSelectedOption = static_cast<Option>((selectedOption + 1) % optionNum);
+        else if (IsKeyPressed(KEY_UP)) this->currentSelectedOption = static_cast<Option>(abs((selectedOption - 1) % optionNum));
+        else if (IsKeyPressed(KEY_ENTER)) {
+            Screen *nextScreen = this->currentSelectedOption == Option::NextRun ? (Screen *)(new GameplayScreen()) : (Screen *)(new TitleScreen(false));
+            current->changeCurrentScreen(nextScreen);
+        }
+    }
 }
 
 void EndingScreen::draw() {
+    ClearBackground(BLACK);
 
-    ClearBackground(RAYWHITE);
-    DrawText("Ending Screen", 190, 200, 20, LIGHTGRAY);
+    if (this->currentFrame < TotalFramesNum) {
+        Animations::FadeText("You died", 190, 330, 20, LIGHTGRAY, currentFrame, FadeInFramesNum, VisibleFramesNum, FadeOutFramesNum);
+        this->currentFrame++;
+    }
+    else {
+        DrawText("You died", 190, 330, 20, LIGHTGRAY);
+        DrawText(currentSelectedOption == Option::NextRun ? "Try again <-" : "Try again", 190, 350, 20, LIGHTGRAY);
+        DrawText(currentSelectedOption == Option::BackToTitle ? "Back to title <-" : "Back to title", 190, 370, 20, LIGHTGRAY);
+    }
 
 }
--- EndingScreen.h
+++ EndingScreen.h
@@ -4,5 +4,10 @@
 class EndingScreen : public Screen {
 public:
     EndingScreen();
+    void updateGameStatus(GameStatus *current);
     void draw();
+private:
+    enum class Option : int { NextRun = 0, BackToTitle, End };
+    int currentFrame;
+    Option currentSelectedOption;
 };
